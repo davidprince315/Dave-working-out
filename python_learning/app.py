740b50ca4afe7548f24b0fe6ce3d695271992f8d
David Prince <davidprince@Davids-Air.lan>
2024-10-15 17:15:35 -0400

    code learning

--- python_learning/app.py
+++ python_learning/app.py
@@ -1,25 +1,111 @@
+#must create classes for Node and Linked List and manually define functions for each 
+#because Nodes and linked lists aren't inherhitly found in python
 
-# 2. You have a list of your favourite marvel super heros
-# heros=['spider man','thor','hulk','iron man','captain america']
-# Using this list
+#must create a class for the node
+class Node:
+	def __init__(self, data=None, next=None):
+		self.data = data
+		self.next = next
 
-heros=['spider man','thor','hulk','iron man','captain america']
-# 1. Length of the list
-print(len(heros))
-# 2. Add 'black panther' at the end of this list
-heros.append('black panther')
-print(heros)
-# 3. You realize that you need to add 'black panther' after 'hulk',
-# so remove it from the list first and then add it after 'hulk'
-heros.remove('black panther')
-heros.insert(3,'black panther')
-print(heros)
-# 4. Now you don't like thor and hulk because they get angry easily :)
-#    So you want to remove thor and hulk from list and replace them with doctor strange (because he is cool).
-#    Do that with one line of code.
-heros[1:3]=['doctor strange']
-print(heros)
-# 5. Sort the list in alphabetical order
-heros.sort()
-print(heros)
-  
+#must create a class for the linked list
+class LinkedList:
+	def __init__(self):
+		self.head = None
+
+	def print(self):
+		if self.head == None:
+			print("List is Empty")
+			return
+		itr = self.head
+		ll_str = " "
+		while itr:
+			ll_str = str(itr.data)+ '--->' if itr.next else str(itr.data)
+			itr = itr.next
+		print(llstr)
+
+	def getLength(self):
+		itr = self.head
+		count = 0
+		while itr:
+			count +=1
+			itr = itr.next
+		print(f"The total nodes in the linked list is {count}")
+
+	def insertBeggining(self, data):
+		node = Node(data, self.head)
+		self.head = node
+
+
+	def insertEnd(self, data):
+		if self.head is None:
+			self.head = Node(data, None)
+		
+		itr = self.head
+
+		while itr:
+			itr = itr.next
+
+		itr.next Node(data, None)
+
+	def insertAt(self, index, data):
+		if index < 0 or index > self.getLength():
+			raise Exception("Invalid Index")
+
+		if index == 0:
+			self.insertBeggining(data)
+			return
+
+		itr = self.head
+		count = 0
+
+		while itr:
+			if count == index - 1:
+				node = Node(data, itr.next)
+				break
+
+			itr = itr.next
+			count += 1
+
+	def removeAt():
+		if index < 0 or index > self.getLength():
+			raise Exception("Invalid Index")
+
+		if index == 0:
+			self.head = self.head.next
+			return
+
+		itr = self.head
+		count = 0
+
+		while itr:
+			if count == index - 1:
+				itr.next = itr.next.next
+				break
+
+			itr = itr.next
+			count += 1
+
+	def insertValues():
+		self.head = None
+		for data in data_list:
+			self.insertEnd(data)
+
+
+    ll = LinkedList()
+    ll.insert_values(["banana","mango","grapes","orange"])
+    ll.print()
+    ll.insert_after_value("mango","apple") # insert apple after mango
+    ll.print()
+    ll.remove_by_value("orange") # remove orange from linked list
+    ll.print()
+    ll.remove_by_value("figs")
+    ll.print()
+    ll.remove_by_value("banana")
+    ll.remove_by_value("mango")
+    ll.remove_by_value("apple")
+    ll.remove_by_value("grapes")
+    ll.print()
+
+
+
+
