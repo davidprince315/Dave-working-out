eab92e724aeb8427608c0193508c2029c3503bb6
Dave <davidprince315@yahoo.com>
2024-12-17 22:58:44 -0500

    added texture/font edits

--- python_learning/app.py
+++ python_learning/app.py
@@ -7,18 +7,19 @@ class HashTable:
 		h = 0
 		for char in key:
 			h+= ord(char)
-			return h % self.MAX
+		return h % self.MAX
 
 	def add(self, key, val):
 		h = self.get_hash(key)
+		found = False
 
 		for idx, element in enumerate(self.arr[h]):
 			if len(element)==2 and element[0]==key:
 				self.arr[h][idx] = (key,val)
 				found = True
 				break
-			if not found:
-				self.arr[h].append((key,val))
+		if not found:
+			self.arr[h].append((key,val))
 		
 
 	def get(self, key):
@@ -40,10 +41,17 @@ with open("nyc_weather.csv","r") as f:
 	for line in f:
 		tokens = line.split(',')
 		day = tokens[0]
+		print (day)
 		if tokens[1].isdigit():
 			temp = int(tokens[1])
 		else:
 			temp = tokens[1]
+		print(temp)
 		table.add(day,temp)
-print(table.arr)
+		print(table.arr)
+
+average_temp = (get(Jan 1) + get(Jan 2) + get(Jan 3)+get(Jan 4)+get(Jan 5)+get(Jan 6)+get(Jan 7))/7
+print(average_temp)
+
+
 
